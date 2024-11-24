b7186ebd19a837106c55d12ef1916fcabd5acf38
gerti1991 <gskerja@gmail.com>
2024-11-24 22:54:44 +0000

    Section 2 add it the doc.  3

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,14 +2,23 @@
 
 """
 This module contains the `matrix_shape` function.
-The function calculates the shape of a matrix by
- determining the dimensions at each level.
+
+The function calculates the shape of a matrix by determining the dimensions at
+each level.
 It ensures all rows at the same depth have consistent lengths.
 """
 
 
 def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix.
 
+    Args:
+        matrix (list): A nested list representing the matrix.
+
+    Returns:
+        list: A list of integers representing the dimensions of the matrix.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
