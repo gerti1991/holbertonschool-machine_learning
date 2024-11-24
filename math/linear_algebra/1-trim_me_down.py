ab977bed25f20e742379cf0aad173018b5f846b6
gerti1991 <gskerja@gmail.com>
2024-11-24 13:14:03 +0000

    1. Trim Me Down v4

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
-the_middle = [[matrix[0][2], matrix[0][3]], [matrix[1][2], matrix[1][3]],
-              [matrix[2][2], matrix[2][3]]]
+for row in matrix:
+    the_middle.append([row[2], row[3]])
 print("The middle columns of the matrix are: {}".format(the_middle))
