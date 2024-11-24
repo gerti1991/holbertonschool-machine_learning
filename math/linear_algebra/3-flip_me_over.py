41925c4a6abc4bf40d8b650034ab556377133510
gerti1991 <gskerja@gmail.com>
2024-11-24 23:40:47 +0000

    3. Flip Me Over

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def matrix_transpose(matrix):
+    """
+    Returns the transpose of a 2D matrix.
+
+    Args:
+        matrix (list of lists): A 2D matrix to transpose.
+
+    Returns:
+        list of lists: A new 2D matrix representing the transpose
+         of the input matrix.
+    """
+    shape = []
+
+    for row in matrix:
+        shape.append(len(row))
+
+    if len(set(shape)) != 1:
+        return "Error you put a list with different shapes"
+    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
