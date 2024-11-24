8c29a94350b9c2ab74feb541bd8aede1a542a67e
gerti1991 <gskerja@gmail.com>
2024-11-24 22:22:24 +0000

    Section 2 add it the doc.

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix.
+
+    Args:
+        matrix (list): A list of lists representing the matrix.
+    
+    Returns:
+        list: A list of integers representing the dimensions of the matrix.
+              If rows are inconsistent in length, the function returns "Inconsistent row lengths".
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
