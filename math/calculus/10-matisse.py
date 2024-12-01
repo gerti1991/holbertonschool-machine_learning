1cde3909ddfb6fa27f6c1b447524bea5719b5236
gerti1991 <gskerja@gmail.com>
2024-12-01 19:45:42 +0000

    10

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Derive happiness in oneself from a good day's work
+"""
+
+
+def poly_derivative(poly):
+    """
+    Derive happiness in oneself from a good day's work
+    """
+    if not isinstance(poly, list):
+        return None
+    if len(poly) == 1:
+        return [0]
+    der_poly = [poly[i] * i for i in range(1, len(poly))]
+    return der_poly
