0e94cab5773ccb65b829510a80127c9ad4b1c7cb
gerti1991 <gskerja@gmail.com>
2024-12-02 14:39:47 +0000

    17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -12,11 +12,12 @@ def poly_integral(poly, C=0):
         return None
     if len(poly) == 0:
         return [C]
-    int_poly = [C]
-    for power, coff in enumerate(poly):
-        n_coff = coff/(power+1)
-        if n_coff - int(n_coff) == 0:
-            int_poly.append(int(n_coff))
-        else:
-            int_poly.append(n_coff)
-    return int_poly
+    else:
+        int_poly = [C]
+        for power, coff in enumerate(poly):
+            n_coff = coff/(power+1)
+            if n_coff - int(n_coff) == 0:
+                int_poly.append(int(n_coff))
+            else:
+                int_poly.append(n_coff)
+        return int_poly
