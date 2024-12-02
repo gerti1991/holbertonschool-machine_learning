5530ad83a0525064c0139e65efb1cbb26e412d92
gerti1991 <gskerja@gmail.com>
2024-12-02 14:12:33 +0000

    17

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Integrate happiness in oneself from a good day's work
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+    Integrate happiness in oneself from a good day's work
+    """
+    if not isinstance(poly, list):
+        return None
+    if len(poly) == 1:
+        return [C]
+    if len(poly) == 0:
+        return None
+    int_poly = [C]
+    for power, coff in enumerate(poly):
+        int_poly.append(coff/(power+1))
+    return int_poly
