307b0ec556ff3369ea94ee0ae705ad5da762b003
gerti1991 <gskerja@gmail.com>
2024-12-15 18:27:46 +0000

    11

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -3,15 +3,15 @@
 Test
 """
 
-import pandas as pd
+index = __import__('10-index').index
+
 
 def concat(df1, df2):
     """
     Test
     """
-    df_concat = pd.concat(
-                        [df2[df2['Timestamp'] <= 1417411920], df1],
-                        keys=['bitstamp', 'coinbase']
-                        )
-    df = df_concat
-    return df
+    df1 = index(df1)
+    df2 = index(df2)
+    df2_filtered = df2[df2.index <= 1417411920]
+    df_concat = pd.concat([df2_filtered, df1], keys=['bitstamp', 'coinbase'])
+    return df_concat
