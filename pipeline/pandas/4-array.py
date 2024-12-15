4786df6beb3bf8373c27c530ac0f6403551a09a9
gerti1991 <gskerja@gmail.com>
2024-12-15 17:30:51 +0000

    4

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -3,13 +3,12 @@
 Test
 """
 
-import pandas as pd
+import numpy as np
 
 
 def array(df):
     """
     Test
     """
-    df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
     A = (df[['High', 'Close']].tail(10)).to_numpy()
     return A
