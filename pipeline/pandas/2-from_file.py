0bd3694d51a5594631f797eedc1aedbf779e15fa
gerti1991 <gskerja@gmail.com>
2024-12-15 17:06:41 +0000

    2

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 import pandas as pd
+"""
+Test
+"""
+
 
 def from_file(filename, delimiter):
-    
+    """
+    Test
+    """
     df = pd.read_csv(filename, delimiter=delimiter)
-    
     return df
