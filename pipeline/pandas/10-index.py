8db622234dcee930a6c4f2903bc8d2fb29ac5f63
gerti1991 <gskerja@gmail.com>
2024-12-15 18:10:30 +0000

    10

--- pipeline/pandas/10-index.py
+++ pipeline/pandas/10-index.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+"""
+Test
+"""
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
-df.set_index('Timestamp', inplace=True)
-
-print(df.tail())
+def index(df):
+    """
+    Test
+    """
+    df.set_index('Timestamp', inplace=True)
+    return df
