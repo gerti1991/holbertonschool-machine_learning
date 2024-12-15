ac1a1fa79adf4c4efc45ba3354ce231ddcf8b84f
gerti1991 <gskerja@gmail.com>
2024-12-15 17:02:00 +0000

    1

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -6,14 +6,10 @@ Comment
 import pandas as pd
 
 
-def df():
-    """
-    comment
-    """
-    data = {
-        'First': [0.0, 0.5, 1.0, 1.5],
-        'Second': ['one', 'two', 'three', 'four']
-    }
 
-    df = pd.DataFrame(data, index=['A', 'B', 'C', 'D'])
-    return df
+data = {
+    'First': [0.0, 0.5, 1.0, 1.5],
+    'Second': ['one', 'two', 'three', 'four']
+}
+
+df = pd.DataFrame(data, index=['A', 'B', 'C', 'D'])
