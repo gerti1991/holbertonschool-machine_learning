4f2ea78d59d45e0be8dcc7eb308fe9bda00ef9ac
gerti1991 <gskerja@gmail.com>
2024-12-15 17:34:25 +0000

    5

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -9,5 +9,5 @@ def array(df):
     Test
     """
     last_10 = df[['High', 'Close']][-10:]
-    result_list = [list(row) for row in last_10_rows]
+    result_list = [list(row) for row in last_10]
     return result_list
