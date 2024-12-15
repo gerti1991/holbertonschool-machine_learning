d3599b24336cc8cbc320e25a6b4bb9286854eea2
gerti1991 <gskerja@gmail.com>
2024-12-15 18:37:49 +0000

    12

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -13,7 +13,11 @@ def hierarchy(df1, df2):
     """
     df1 = index(df1)
     df2 = index(df2)
-    df2_filtered = df2[(df2.index >= 1417411980) & (df2.index <= 1417411980)]
-    df_concat = pd.concat([df2_filtered, df1], keys=['bitstamp', 'coinbase'])
+    df2_filtered = df2[(df2.index >= 1417411980) & (df2.index <= 1417417980)]
+    df1_filtered = df1[(df1.index >= 1417411980) & (df1.index <= 1417417980)]
+    df_concat = pd.concat(
+                        [df2_filtered, df1_filtered],
+                        keys=['bitstamp', 'coinbase']
+                        )
     df_concat = df_concat.swaplevel(0, 1).sort_index(level=0)
     return df_concat
