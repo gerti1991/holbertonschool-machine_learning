3125675a4cc559224b5c6bfe7b134d654fb37033
gerti1991 <gskerja@gmail.com>
2024-12-16 16:51:58 +0000

    0

---
+++ math/plotting/0-line.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Test
+"""
+
+
+def line():
+    """
+    Test
+    """
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(np.arange(0, 11), y, 'r-', linewidth=2)
+    plt.xlim(0, 10)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("y = x^3")
+    plt.show()
