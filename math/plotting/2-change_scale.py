eb59b4e46e35b556a83636194c7399d4d98e0293
gerti1991 <gskerja@gmail.com>
2024-12-17 15:58:33 +0000

    2-plot

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -16,7 +16,7 @@ def change_scale():
     t = 5730
     y = np.exp((r / t) * x)
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(x, y, c='blue')
+    plt.plot(x, y)
     plt.xlabel('Time (years)')
     plt.ylabel('Fraction Remaining')
     plt.title('Exponential Decay of C-14')
