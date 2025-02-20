9dbba2f76dc6e3317835dc5e4fbbac5284e381c5
gerti1991 <gskerja@gmail.com>
2025-02-20 14:39:01 +0100

    asdasdad

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -14,21 +14,11 @@ def pca(X, var=0.95):
         W: numpy.ndarray of shape (d, nd), weights matrix maintaining
            var fraction of X's variance
     """
-    # Covariance matrix
     cov = np.cov(X, rowvar=False)
-
-    # Eigen decomposition
     eig_vals, eig_vecs = np.linalg.eigh(cov)
-
-    # Sort descending
     idx = np.argsort(eig_vals)[::-1]
     eig_vals = eig_vals[idx]
     eig_vecs = eig_vecs[:, idx]
-
-    # Cumulative variance
     cum_var = np.cumsum(eig_vals) / np.sum(eig_vals)
-
-    # Number of components
     n_components = np.argmax(cum_var >= var) + 1
-
     return eig_vecs[:, :n_components]
