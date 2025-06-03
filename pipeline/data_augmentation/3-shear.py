bf99afddd7adf520507f6ef27a3fa227f63452d3
gerti1991 <gskerja@gmail.com>
2025-06-03 18:57:49 +0200

    3-1

--- pipeline/data_augmentation/3-shear.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""shear image"""
-import tensorflow as tf
-
-
-def shear_image(image, intensity):
-    """that randomly shears an image"""
-    return tf.keras.preprocessing.image.random_shear(image, intensity)
