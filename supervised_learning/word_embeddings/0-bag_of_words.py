f2dbc36d54e738c3a90818d358d639de2f54f88a
gerti1991 <gskerja@gmail.com>
2025-07-21 14:35:18 +0200

    emb-0

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -7,5 +7,5 @@ def bag_of_words(sentences, vocab=None):
     """creates a bag of words embedding matrix"""
     cont_vector = CountVectorizer(vocabulary=vocab)
     embed = cont_vector.fit_transform(sentences)
-    features = cont_vector.get_feature_names()
+    features = cont_vector.get_feature_names_out()
     return embed.toarray(), features
